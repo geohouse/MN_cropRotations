1c1e93043998aaa5c3a9298a4465f4b52b0c7c8a
geohouse <geo.house7@gmail.com>
2022-05-28 18:34:43 -0600

    more tests

--- js/main_mapboxTests.js
+++ js/main_mapboxTests.js
@@ -3,7 +3,89 @@ mapboxgl.accessToken='pk.eyJ1IjoiZ2VvaG91c2UiLCJhIjoiY2wza2RhZXByMGpvNjNibHB6MDN
 const stateToCountyZoomThresh = 7;
 const countyToTRZoomThresh = 9;
 const TRtoSectionZoomThresh = 11;
-const maxZoomLevel = 22;
+const maxZoomLevel = 12; // was 22
+
+
+
+let geojsonURL = "https://raw.githubusercontent.com/geohouse/MN_cropRotations/main/geojson/MN_countyCentroids_CRS4326_v3.geojson";
+//let jsonURL = "https://raw.githubusercontent.com/geohouse/MN_cropRotations/main/mapBox_pointImageTest.geojson";
+
+// Layers are rendered in the same order they are defined; layers near the top of the file are rendered first (i.e. under)
+// layers at the bottom of the file
+
+// currentBackgroundLayer = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}',{
+//     maxZoom:16,
+//     attribution: 'Map tiles by <a href="https://usgs.gov">Department of Interior/USGS</a> | <a href="https://github.com/geohouse/blockIslandGlassFloats">This site\'s source code</a>',
+// });
+
+// currentBackgroundLayer.addTo(map);
+
+// function renderLayer(jsonData){
+//     const renderedLayer = L.geoJson(jsonData, {
+//             pointToLayer: function(geoJsonPoint, latlng) {
+//             //console.log("The point is:");
+//             //console.log(geoJsonPoint);
+            
+//             // This is the url coded for each feature in the geojson file.
+//             const pathToIcon = geoJsonPoint.properties.imgUrl;
+//             console.log(pathToIcon);
+//             console.log(latlng);
+//             //const iconSizeVar = [100,100];
+//             // Location in the icon that is the specified geographic location that it's 
+//             // marking (i.e. the 'tip' of the icon on the map). This is in pixels and 
+//             // is relative to the top left corner of the icon [x,y]
+//             //const iconAnchorVar = [50,50];
+
+//             let mapIcon = L.icon({
+//                 //"css/images/marker-icon.png"
+                
+//                 iconUrl: pathToIcon,
+//                 opacity: 0,
+//                 // Size [x,y] in pixels
+//                 //iconSize: iconSizeVar,
+//                 // Location in the icon that is the specified geographic location that it's 
+//                 // marking (i.e. the 'tip' of the icon on the map). This is in pixels and 
+//                 // is relative to the top left corner of the icon [x,y]
+//                 //iconAnchor: iconAnchorVar
+//                 //popupAnchor: 
+//             });
+//             return L.marker(latlng, {icon: mapIcon});
+//         }, onEachFeature: function(feature, layer){
+//             let countyName = feature.properties.COUN_LC;
+//             let lat = feature.geometry.coordinates[1];
+//             let lng = feature.geometry.coordinates[0];
+//             L.marker([lat,lng], L.icon({iconUrl: feature.properties.imgUrl})).bindPopup('County Name: ' + '<b>' + countyName);
+//         }
+//     });
+//     return renderedLayer;
+// }
+
+// let countyPointLayer;
+
+// function plotPoints(urlForData){ 
+// $.getJSON(urlForData, function(jsonData){
+//     // returns JSON as an object
+//     console.log(typeof(jsonData));
+
+//     console.log(jsonData.length);
+    
+//     //for (i = 0; i < jsonData.length; i = i+1){
+//     //    console.log(jsonData[i].properties);
+//         // properties: year, numFound, floatType, locationName
+//     //}
+//     countyPointLayer = renderLayer(jsonData);
+    
+//     countyPointLayer.addTo(map);
+//     }
+// )
+// }
+
+// plotPoints(jsonURL);
+
+
+
+
+
 
 
 // Layers are rendered in the same order they are defined; layers near the top of the file are rendered first (i.e. under)
@@ -18,8 +100,37 @@ const map = new mapboxgl.Map({
     zoom: 6
 });
 
+function plotURLIcons (inputGeojson){
+    for (marker of inputGeojson.features){
+        console.log("Marker");
+        console.log(marker);
+        let iconDiv = document.createElement('div');
+        const width = 40;
+        const height = 40;
+        iconDiv.className = 'icon'; 
+        iconDiv.style.backgroundImage = 'url(https://github.com/geohouse/MN_cropRotations/blob/main/img/cnty/Hennepin.png)';
+        //iconDiv.style.width = `${width}px`;
+        //iconDiv.style.height = `${height}px`;
+        console.log(iconDiv);
+        new mapboxgl.Marker(iconDiv).setLngLat(marker.geometry.coordinates).addTo(map);
+    }
+}
+
 // Wait until map has finished loading
 map.on('load', () => {
+
+    map.addSource('countyCentroids_TEST', {
+        data: geojsonURL,
+        type: 'geojson'
+    });
+    let geojsonData;
+    $.getJSON(geojsonURL, function(jsonData){
+        plotURLIcons(jsonData);
+    });
+
+    console.log("Testing");
+    console.log(geojsonData);
+
 
     // Add my custom vector tilesets
 
