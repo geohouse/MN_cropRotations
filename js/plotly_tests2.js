4d3f0d4168892566f81b7ac703ede451ccc9f926
Geoffrey House <geo.house7@gmail.com>
2022-07-04 13:18:25 -0600

    plotly lines with labels working

--- js/plotly_tests2.js
+++ js/plotly_tests2.js
@@ -21,13 +21,13 @@ const xLookup = {1:2008,2:2009,3:2010,4:2011,5:2012,6:2013,7:2014,8:2015,9:2016,
 // y lookup keys have to be strings because the y values are floats. This is for the marker lookup
 const yLookup = {"1":'Other',"1.9":'Dry beans', "2.8":'Sugar beets', "3.7":'Spring wheat', "4.6":'Hay', "5.5":'Soybeans', "6.4":'Corn'};
 // The y lookup for lines is more complicated because there are 49 possibilities (7 crops to/from all 7 crops)
-const yLookup_lines = {"1":'Other',"1.8":'Other',"2.6":'Other',"3.4":'Other',"4.2":'Other',"5.0":'Other',"5.8":'Other',
+const yLookup_lines = {"1":'Other',"1.8":'Other',"2.6":'Other',"3.4":'Other',"4.2":'Other',"5":'Other',"5.8":'Other',
                         "1.1":'Dry beans',"1.9":'Dry beans',"2.7":'Dry beans',"3.5":'Dry beans',"4.3":'Dry beans',"5.1":'Dry beans',"5.9":'Dry beans',
-                        "1.2":'Sugar beets',"2.0":'Sugar beets',"2.8":'Sugar beets',"3.6":'Sugar beets',"4.4":'Sugar beets',"5.2":'Sugar beets',"6.0":'Sugar beets',
+                        "1.2":'Sugar beets',"2":'Sugar beets',"2.8":'Sugar beets',"3.6":'Sugar beets',"4.4":'Sugar beets',"5.2":'Sugar beets',"6":'Sugar beets',
                         "1.3":'Spring wheat',"2.1":'Spring wheat',"2.9":'Spring wheat',"3.7":'Spring wheat',"4.5":'Spring wheat',"5.3":'Spring wheat',"6.1":'Spring wheat',
-                        "1.4":'Hay',"2.2":'Hay',"3.0":'Hay',"3.8":'Hay',"4.6":'Hay',"5.4":'Hay',"6.2":'Hay',
+                        "1.4":'Hay',"2.2":'Hay',"3":'Hay',"3.8":'Hay',"4.6":'Hay',"5.4":'Hay',"6.2":'Hay',
                         "1.5":'Soybeans',"2.3":'Soybeans',"3.1":'Soybeans',"3.9":'Soybeans',"4.7":'Soybeans',"5.5":'Soybeans',"6.3":'Soybeans',
-                        "1.6":'Corn',"2.4":'Corn',"3.2":'Corn',"4.0":'Corn',"4.8":'Corn',"5.6":'Corn',"6.4":'Corn',
+                        "1.6":'Corn',"2.4":'Corn',"3.2":'Corn',"4":'Corn',"4.8":'Corn',"5.6":'Corn',"6.4":'Corn',
                     }
 // let markers = {
 //     x: [1,1,1,2,2,2,3,3,3],
@@ -243,6 +243,15 @@ function processData_lines(inputData){
     // later date
     let currChunk_yearFrom = 0;
     let currChunk_yearTo = 0;
+
+    // For y values, the 1st row in the chunk is the 'core' crop that the line's either going from or to (depending on the direction)
+    // and the 3rd row is the 'satellite' crop that the line has arrived from or has gone to (depending on direction)
+    let currChunk_coreCrop = '';
+    let currChunk_satelliteCrop = '';
+    //Once the order is known (from yearFrom/to), then assign the crops to from/to
+    let currChunk_fromCrop = '';
+    let currChunk_toCrop = '';
+
     //console.log(inputData);
     // The line data is given in 3-row chunks, where every 3 rows represents a new line
     for(let i=1; i<= (inputData.length / 3); i++){
@@ -255,11 +264,17 @@ function processData_lines(inputData){
             // If this is the first row in the chunk, then get the color and the size to use
             if(chunkOffset === 0){
                 currChunk_color = currRow['colorToUse'];
-                currChunk_size = currRow['width'] * 10;
-                currChunk_year1 = xLookup[Number.parseInt(currRow['plotXAxisTo'])]
+                currChunk_size = currRow['width'];
+                currChunk_year1 = xLookup[Number.parseInt(currRow['xCoords'])]
+                currChunk_coreCrop = yLookup_lines[currRow['yCoords']];
             }
 
-            if(chunkOffset === 1){
+
+            currChunk_xCoords.push(currRow['xCoords']);
+            currChunk_yCoords.push(currRow['yCoords']);
+
+            if(chunkOffset === 2){
+                currChunk_satelliteCrop = yLookup_lines[currRow['yCoords']];
                 // This will only be true if the xCoords for the curr row in the chunk are > xCoords in the first
                 // row of the chunk, which means when using floor, they will return the same year from the xLookup.
                 // In this case, currChunk_year2 = currChunk_year1 + 1
@@ -272,16 +287,17 @@ function processData_lines(inputData){
                 if(currChunk_year1 > currChunk_year2){
                     currChunk_yearFrom = currChunk_year2;
                     currChunk_yearTo = currChunk_year1;
+                    currChunk_toCrop = currChunk_coreCrop;
+                    currChunk_fromCrop = currChunk_satelliteCrop;
+
                 } else{
                     currChunk_yearFrom = currChunk_year1;
                     currChunk_yearTo = currChunk_year2;
+                    currChunk_fromCrop = currChunk_coreCrop;
+                    currChunk_toCrop = currChunk_satelliteCrop;
+
                 }
-            }
 
-            currChunk_xCoords.push(currRow['xCoords']);
-            currChunk_yCoords.push(currRow['yCoords']);
-
-            if(chunkOffset === 2){
                 currChunkHolder_line = {
                     x: currChunk_xCoords,
                     y: currChunk_yCoords,
@@ -289,9 +305,9 @@ function processData_lines(inputData){
                     connectgaps: false,
                     line: {
                         color: currChunk_color,
-                        width:currChunk_size / 20
-                    }
-                    //text: ``
+                        width:currChunk_size / 5
+                    },
+                    text: `From: ${currChunk_fromCrop} (${currChunk_yearFrom}) <br> To: ${currChunk_toCrop} (${currChunk_yearTo}) <br> Covered<br>${currChunk_size}% of area`
                 }
                 lineOutputCombined.push(currChunkHolder_line);
                 if(i < 6){
@@ -306,6 +322,10 @@ function processData_lines(inputData){
                 currChunk_year2 = 0;
                 currChunk_yearFrom = 0;
                 currChunk_yearTo = 0;
+                currChunk_coreCrop = '';
+                currChunk_satelliteCrop = '';
+                currChunk_fromCrop = '';
+                currChunk_toCrop = '';
             }
             //If this is the last row in the chunk, then build the plotly info for this block and push it to the 
             // holder.
@@ -313,17 +333,17 @@ function processData_lines(inputData){
         }
     }
     
-    let testMarker = {
-        x: [3],
-        y: [4],
-        type: 'scatter',
-        mode: 'markers',
-        marker: {
-            color: '#FFFF00',
-            size: 50
-        }
-    };
-    console.log(testMarker);
+    // let testMarker = {
+    //     x: [3],
+    //     y: [4],
+    //     type: 'scatter',
+    //     mode: 'markers',
+    //     marker: {
+    //         color: '#FFFF00',
+    //         size: 50
+    //     }
+    // };
+    // console.log(testMarker);
         
         //     x: xMarkTo[i],
         //     y: yMarkFrom[i],
